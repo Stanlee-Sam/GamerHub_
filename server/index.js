ce48a050930a41dc18a366cde6e2401df6f85fa4
Stanley Amunze <stanleyamunze03@gmail.com>
2025-10-21 07:41:23 +0300

    cors update

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["http://localhost:5173", 'https://gamer-hub-phi.vercel.app'],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
